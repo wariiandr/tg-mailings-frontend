006d7634042da9b35159880f1c9e4a93b51dffff
Mariia <wariia.andr@gmail.com>
2024-11-01 20:42:40 +0300

    delete sourcemap in tsconfig

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import instance from "@/api/instance";
-import groups from "@/api/groups";
-import auth from "@/api/auth";
-export default {
-    groups: groups(instance),
-    auth: auth(instance),
-};
-//# sourceMappingURL=index.js.map
